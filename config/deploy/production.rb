180acea1226b7340af64e24bbcabcc1ed051639e
Andrews Peprah <andrews.peprah@nfortics.com>
2016-10-01 16:48:27 +0000

    changed deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '66.175.212.90', user: 'oboafo', roles: %w{web app}
+server '66.175.212.90', user: 'oboafo', roles: [:web, :app, :db], primary: true
 
 
 # role-based syntax
